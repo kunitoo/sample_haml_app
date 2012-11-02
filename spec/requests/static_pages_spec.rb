56c5b5fec887d912a77f431f7c510fe141b5c3c7
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-02 15:37:26 +0900

    StaticPages controller spec with a base title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
@@ -11,7 +13,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                                text: "Ruby on Rails Tutorial Sample App | Home")
+                                text: "#{base_title} | Home")
     end
   end
 
@@ -25,7 +27,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                                text: "Ruby on Rails Tutorial Sample App | Help")
+                                text: "#{base_title} | Help")
     end
   end
 
@@ -39,7 +41,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                                text: "Ruby on Rails Tutorial Sample App | About Us")
+                                text: "#{base_title} | About Us")
     end
   end
 end
