92d4c5896706330019774f47b42e80fcdf0018bb
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-02 13:23:19 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleHamlApp::Application.config.secret_token = 'b0a97206819a4ef88cf5c689273c1f917105c94d5c431b741e42116d51b56e41aae39781cd590262f5b6590b5c44d740c8741636b35cf65348c172fd95973dfd'
