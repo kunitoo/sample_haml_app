293d5c0a3b9bea0eb29d0b8102b40a58919dbdb2
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-02 17:24:41 +0900

    Home pageのタイトルをから"home"を抜いた

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,10 +10,15 @@ describe "StaticPages" do
       page.should have_selector('h1', text: 'Sample App')
     end
 
-    it "should have the right title" do
+    it "should have the base title"do
       visit '/static_pages/home'
       page.should have_selector('title',
-                                text: "#{base_title} | Home")
+                                text: "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', text: '| Home')
     end
   end
 
