a90351b125eed68c9d35038488d2e72893f5e150
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-02 14:31:56 +0900

    add About page rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,4 +16,12 @@ describe "StaticPages" do
       page.should have_content('Help')
     end
   end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
 end
