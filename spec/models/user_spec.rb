b1a21b9632ed3f236a92c1143fbeffd06bebf500
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-06 16:21:26 +0900

    Testing for the password and password_confirmation attributes.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,13 +13,16 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: 'Example User', email: 'user@example.com') }
+  before { @user = User.new(name: 'Example User', email: 'user@example.com',
+                    password: 'foobar', password_confirmation: 'foobar') }
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
