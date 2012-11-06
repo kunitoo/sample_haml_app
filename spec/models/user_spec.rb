d455dfba920b2860b766c837da98e96e6f0e3fb1
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-06 16:34:43 +0900

    Test for the password and password confirmation.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -71,4 +71,19 @@ describe User do
 
     it { should_not be_valid }
   end
+
+  describe 'when password is not present' do
+    before { @user.password = @user.password_confirmation = '' }
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = 'mismatch' }
+    it { should_not be_valid }
+  end
+
+  describe 'when password confirmation is nil' do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
 end
