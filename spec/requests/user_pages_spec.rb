ba3c82f3ef3f9102b577618e47588a2419e422cf
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-06 18:27:25 +0900

    Testing the user show page (with factories)

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,7 +7,16 @@ describe 'UserPages' do
   describe 'signup page' do
     before { visit signup_path }
 
-    it { should have_selector('h1', test: 'Sing up') }
-    it { should have_selector('title', test: full_title('Sign up')) }
+    it { should have_selector('h1', text: 'Sing up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
+  end
+
+  describe 'profile page' do
+    # Code to make a user variable
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1', text: user.name) }
+    it { should have_selector('title', text: user.name) }
   end
 end
