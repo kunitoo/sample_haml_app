791873116246d362357d842e3d90d6e416989f1d
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-06 15:19:34 +0900

    Adding a length validation of the name attribute.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,13 +27,13 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe 'when email is not present' do
-    before { @user.email = '' }
+  describe 'when name is too long' do
+    before { @user.name = 'a' * 51 }
     it { should_not be_valid }
   end
 
-  describe 'when name is too long' do
-    before { @user.name = 'a' * 51 }
+  describe 'when email is not present' do
+    before { @user.email = '' }
     it { should_not be_valid }
   end
 end
