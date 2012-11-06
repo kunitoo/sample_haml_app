b7cc97436ce7fd5ce5cf486ac68e3992eebe7786
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-06 13:16:35 +0900

    The initial spec for users, with a test for the signup page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ SampleHamlApp::Application.routes.draw do
 
   root to: 'static_pages#home'
 
+  match '/signup', to: 'users#new'
+
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
