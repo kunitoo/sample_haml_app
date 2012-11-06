ecfd6638d4150f46954c124ea2827e22902b5e32
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-06 15:12:15 +0900

    Validating the presence of the name and email attribute.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,4 +19,16 @@ describe User do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe 'when name is not present' do
+    before { @user.name = '' }
+    it { should_not be_valid }
+  end
+
+  describe 'when email is not present' do
+    before { @user.email = '' }
+    it { should_not be_valid }
+  end
 end
