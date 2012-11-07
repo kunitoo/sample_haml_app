d7a5f55400b9195365c619c57c4f8e5362a01e95
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-07 17:05:19 +0900

    mistake signin with valid information spec

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,7 +27,7 @@ describe 'Authentication' do
     end
 
     describe 'with valid information' do
-      let(:user) { FactoryGirl.create(@user) }
+      let(:user) { FactoryGirl.create(:user) }
       before do
         fill_in 'Email',    with: user.email
         fill_in 'Password', with: user.password
