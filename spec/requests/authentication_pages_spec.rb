943bb36692f0aec54907fb3bde907262ca3bc18f
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-07 13:54:31 +0900

    The tests for signin failure.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,4 +10,11 @@ describe 'Authentication' do
     it { should have_selector('h1', text: 'Sign in') }
     it { should have_selector('title', text: 'Sign in') }
   end
+
+  describe 'signin' do
+    before { visit signin_path }
+
+    it { should have_selector('title', text: 'Sign in') }
+    it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+  end
 end
