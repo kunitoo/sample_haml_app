d8184bfdaef79288152842113118311f6e02939c
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-07 11:59:40 +0900

    Adding a resource to get the standard RESTful actions for sessions.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -5,7 +5,7 @@ describe 'Authentication' do
   subject { page }
 
   describe 'signin page' do
-    before { visit signup_path }
+    before { visit signin_path }
 
     it { should have_selector('h1', text: 'Sign in') }
     it { should have_selector('title', text: 'Sign in') }
