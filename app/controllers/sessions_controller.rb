3692981b20f593b01677b047d8b9d03a160a2666
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-07 14:40:03 +0900

    Correct code for failed signin.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Sign the user in and redirect to the user's show page.
     else
-      flash[:error] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
