109dffd1a61c821ce11972299ef166be9f9e2d7b
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-07 19:25:52 +0900

    Adding a helper method and a custom RSpec matcher.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,6 +18,7 @@ describe 'Authentication' do
       before { click_button 'Sign in' }
 
       it { should have_selector('title', text: 'Sign in') }
+      it { should have_error_message('Invalid') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
 
       describe 'after visiting another page' do
@@ -28,11 +29,7 @@ describe 'Authentication' do
 
     describe 'with valid information' do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in 'Email',    with: user.email
-        fill_in 'Password', with: user.password
-        click_button 'Sign in'
-      end
+      before { valid_signin(user) }
 
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
