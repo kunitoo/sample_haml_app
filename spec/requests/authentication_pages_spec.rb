212df5f2fd22c711ef71518eaae143ca25cef2ab
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-07 14:36:40 +0900

    Correct tests for signin failure.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -14,8 +14,17 @@ describe 'Authentication' do
   describe 'signin' do
     before { visit signin_path }
 
-    it { should have_selector('title', text: 'Sign in') }
-    it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    describe 'with invalid information' do
+      before { click_button 'Sign in' }
+
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe 'after visiting another page' do
+        before { click_link 'Home' }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end
 
     describe 'with valid information' do
       let(:user) { FactoryGirl.create(@user) }
