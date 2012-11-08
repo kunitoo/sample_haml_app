ee3e11326b770f756e94c230270514fd17a67102
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-08 15:51:19 +0900

    The initial user update action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,4 +21,13 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
 end
