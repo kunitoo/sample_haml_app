87e133b58fa55536803b3796802de5040fc9fde8
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-09 18:27:04 +0900

    Tests for the micropost's user association.

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -9,13 +9,10 @@ describe Micropost do
 
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
+  its(:user) { should == user }
 
   it { should be_valid }
-
-  describe 'when user_id is not present' do
-    before { @micropost.user_id = nil }
-    it { should_not be_valid }
-  end
 
   describe 'accessible attributes' do
     it 'should not allow access to user_id' do
@@ -23,5 +20,10 @@ describe Micropost do
         Micropost.new(user_id: user.id)
       end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
     end
+  end
+
+  describe 'when user_id is not present' do
+    before { @micropost.user_id = nil }
+    it { should_not be_valid }
   end
 end
