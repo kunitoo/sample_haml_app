d03a7b138aac1f5eac2cc1780bcf6e41ebabde5a
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-09 18:20:42 +0900

    A test to ensure that the user_id isn't accessible.

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -3,10 +3,7 @@ require 'spec_helper'
 describe Micropost do
 
   let(:user) { FactoryGirl.create(:user) }
-  before do
-    # This code is wrong!
-    @micropost = Micropost.new(content: 'Lorem ipsum', user_id: user.id)
-  end
+  before { @micropost = user.microposts.build(content: 'Lorem ipsum') }
 
   subject { @micropost }
 
@@ -18,5 +15,13 @@ describe Micropost do
   describe 'when user_id is not present' do
     before { @micropost.user_id = nil }
     it { should_not be_valid }
+  end
+
+  describe 'accessible attributes' do
+    it 'should not allow access to user_id' do
+      expect do
+        Micropost.new(user_id: user.id)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
   end
 end
