f1afa4829c5148d1e2f211274306deead2ec2dea
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-09 20:46:57 +0900

    The Microposts controller create action.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,13 @@ class MicropostsController < ApplicationController
   before_filter :signed_in_user
 
   def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = 'Micropost created!'
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
