b4ebd8e0a5bf65079ed35889b95179e9b10b7ec0
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-09 18:11:46 +0900

    Tests for the validity of a new micropost.

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -12,4 +12,11 @@ describe Micropost do
 
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
+
+  it { should be_valid }
+
+  describe 'when user_id is not present' do
+    before { @micropost.user_id = nil }
+    it { should_not be_valid }
+  end
 end
