5d1ea7cf8261dad32ebc7f9ee8532df16bf0c631
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-09 10:53:13 +0900

    mistake tests for pagination.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,7 +23,7 @@ describe 'UserPages' do
 
       it { should have_selector('div.pagination') }
       it 'should list each user' do
-        User.all.each do |user|
+        User.paginate(page: 1).each do |user|
           page.should have_selector('li', text: user.name)
         end
       end
