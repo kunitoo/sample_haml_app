181e37d9c5f48b4833cff070dbd9afa3c82df489
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-12 18:00:38 +0900

    Implementing the user/relationships has_many association.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: 'follower_id', dependent: :destroy
 
   before_save { self.email.downcase! }
   before_save :create_remember_token
