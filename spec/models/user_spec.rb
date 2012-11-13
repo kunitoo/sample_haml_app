8c97963674cd2291bafe7d081a089e09726b6d24
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-13 11:16:28 +0900

    A test for unfollowing a user.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -176,5 +176,12 @@ describe User do
 
     it { should be_following(other_user) }
     its(:followed_users) { should include(other_user) }
+
+    describe 'and unfollowing' do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
   end
 end
