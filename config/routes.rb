c987394671f70909d13fe4f34ea2bba752f5b8ab
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-13 13:34:01 +0900

    Adding the routes for user relationships.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ SampleHamlApp::Application.routes.draw do
   end
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root to: 'static_pages#home'
 
