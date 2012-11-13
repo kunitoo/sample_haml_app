6c993ee8455520386639d0b2d2fd236d0a1989ba
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-11-13 20:07:43 +0900

    Improving from_users_followed_by.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,8 +7,10 @@ class Micropost < ActiveRecord::Base
 
   default_scope order: 'microposts.created_at DESC'
 
+  # Returns microposts from the users being followed by the given user.
   def self.from_users_followed_by(user)
     followed_user_ids = user.followed_user_ids
-    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    where("user_id IN (:followed_user_ids) OR user_id = :user_id",
+          followed_user_ids: followed_user_ids, user_id: user)
   end
 end
